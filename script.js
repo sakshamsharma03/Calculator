89ab21f9d223e5c29a40bd6470f5bc395999978a
saksham <sharma.saksham0309@gmail.com>
2024-03-12 16:20:58 +0530

    Done

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/*let eql=document.querySelector("#equal");
+let inp=document.querySelector("#dis");
+eql.addEventListener("click",()=>
+{
+    console.log("gand marao");
+   let ans= eval(inp);
+   inp=ans;
+   console.log(inp.innerText);
+})*/
